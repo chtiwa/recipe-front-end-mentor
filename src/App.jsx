2939baf5c65b4d7c79f1f0dbd1f0d2f5a29e27c8
chtiwa <chtiwaa@gmail.com>
2024-01-25 22:22:25 +0100

    added fonts

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center bg-yellow-100 w-screen h-full"></div>
+  )
+}
+
+export default App
